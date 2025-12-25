e38726617e62558dab14354e16b65bc17e8665db
Shreya Patil <patilshreya376@gmail.com>
2025-12-25 17:41:37 +0530

    Adding spring boot and dependencies.

---
+++ src/main/java/com/example/Workforce/Task/Orchestration/System/WorkforceTaskOrchestrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Workforce.Task.Orchestration.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforceTaskOrchestrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkforceTaskOrchestrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Workforce/Task/Orchestration/System/WorkforceTaskOrchestrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Workforce.Task.Orchestration.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkforceTaskOrchestrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
